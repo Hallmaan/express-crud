65baa6601d03ada7914de46418846a6d2162fa04
Agung Maliki <agungmaliki@Agungs-MacBook-Air.local>
2018-10-07 20:30:04 +0700

    ini bro

--- model/usersModel.js
+++ model/usersModel.js
@@ -2,7 +2,7 @@ const db = require('../lib/database');
 
 module.exports = {
   getUser: async function(userId) {
-  const rows = await db.executeQuery(`select * from ms_customer where id = '${userId}'`)
+  const rows = await db.executeQuery(`select * from ms_vendor where id = '${userId}'`)
   const user = rows[0]
 
   return user
